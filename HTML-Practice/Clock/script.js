a434d4a982c540800c35aa855739f7297a02c9d0
SahuAman03 <sahuaman212@gmail.com>
2024-04-30 23:37:49 +0530

    first commit

---
+++ HTML-Practice/Clock/script.js
@@ -0,0 +1,11 @@
+
+  function updateClock() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const timeString = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    document.getElementById('clock').innerHTML = timeString;
+  }
+  
+  setInterval(updateClock, 1000); // update clock every 1 second
