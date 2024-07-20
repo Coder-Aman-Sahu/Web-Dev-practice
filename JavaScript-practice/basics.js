6d99714b63b12ff33a8b4595c20c44b36b7af975
Aman Sahu <113002116+Coder-Aman-Sahu@users.noreply.github.com>
2024-07-20 20:06:40 +0530

    Create basics.js

---
+++ JavaScript-practice/basics.js
@@ -0,0 +1,9 @@
+let myFirstVariable = 1;
+myFirstVariable = 'Some string';
+myFirstVariable = new SomeComplexClass();
+const MY_FIRST_CONSTANT = 10;
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+add(1, 3);
